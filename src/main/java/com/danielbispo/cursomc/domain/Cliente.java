0fd054b759726f0dfb081f7093f85491d800e77e
Daniel Bispo <danielvbispo@outlook.com>
2019-02-04 15:59:12 -0200

    Endpoint /clientes/{id} disponível

--- src/main/java/com/danielbispo/cursomc/domain/Cliente.java
+++ src/main/java/com/danielbispo/cursomc/domain/Cliente.java
@@ -15,6 +15,7 @@ import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
 import com.danielbispo.cursomc.domain.enums.TipoCliente;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Cliente implements Serializable{
@@ -29,6 +30,7 @@ public class Cliente implements Serializable{
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
+	@JsonManagedReference
 	@ManyToMany(mappedBy="cliente")
 	private List<Endereco> enderecos = new ArrayList<>();;
 	
