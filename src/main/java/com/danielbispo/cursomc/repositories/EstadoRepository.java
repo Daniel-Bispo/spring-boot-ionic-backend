83652529f7057c9e13a3617b038e8d17f2f7c1f8
Daniel Bispo <danielvbispo@outlook.com>
2019-02-04 14:58:04 -0200

    Estado e Cidade

---
+++ src/main/java/com/danielbispo/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.danielbispo.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.danielbispo.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+}
