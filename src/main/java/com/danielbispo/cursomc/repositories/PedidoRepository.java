86f0876607ea394aec03356e7a372f7a1c9d9798
Daniel Bispo <danielvbispo@outlook.com>
2019-02-04 19:37:51 -0200

    Pedido, Pagamento e EstadoPagamento

---
+++ src/main/java/com/danielbispo/cursomc/repositories/PedidoRepository.java
@@ -0,0 +1,11 @@
+package com.danielbispo.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.danielbispo.cursomc.domain.Pedido;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
+
+}
