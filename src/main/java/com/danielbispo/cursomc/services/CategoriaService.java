63a6f0250fb30048119c4206db0761fa2e49bc7b
Daniel Bispo <danielvbispo@outlook.com>
2019-02-05 15:44:24 -0200

    Validação sintática com Bean Validation

--- src/main/java/com/danielbispo/cursomc/services/CategoriaService.java
+++ src/main/java/com/danielbispo/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.danielbispo.cursomc.domain.Categoria;
+import com.danielbispo.cursomc.dto.CategoriaDTO;
 import com.danielbispo.cursomc.repositories.CategoriaRepository;
 import com.danielbispo.cursomc.services.exceptions.DataIntegrityException;
 import com.danielbispo.cursomc.services.exceptions.ObjectNotFoundException;
@@ -56,5 +57,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
