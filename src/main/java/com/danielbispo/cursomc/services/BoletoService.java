a03967add0772652fce17754275d778fd071ec59
Daniel Bispo <danielvbispo@outlook.com>
2019-02-06 13:13:27 -0200

    Inserindo pedido

---
+++ src/main/java/com/danielbispo/cursomc/services/BoletoService.java
@@ -0,0 +1,19 @@
+package com.danielbispo.cursomc.services;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.springframework.stereotype.Service;
+
+import com.danielbispo.cursomc.domain.PagamentoComBoleto;
+
+@Service
+public class BoletoService {
+
+	public void preencherPagamentoComBoleto(PagamentoComBoleto pagto, Date instanteDoPedido) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(instanteDoPedido);
+		cal.add(Calendar.DAY_OF_MONTH, 7);
+		pagto.setDataVencimento(cal.getTime());
+	}
+}
